6f82e6ed52a4b8a1e5123154679b6d24d43ff622
KhoaZero123 <khoazero123@gmail.com>
2017-06-19 15:36:57 +0700

    Use json to return response when add/remove member in class

--- wp-content/plugins/fgc-quiz-custom-post/fgc-quiz-custom-post.php
+++ wp-content/plugins/fgc-quiz-custom-post/fgc-quiz-custom-post.php
@@ -154,9 +154,8 @@ class FGCQuizClass { /**
         ); 
         $list_users = get_users( $args );
         //echo '<pre>';var_dump($list_users);echo '</pre>';
-        if(!empty($list_users)) {
-         ?>
-        <table class="wp-list-table widefat fixed striped posts" style="width:100%">
+        //if(!empty($list_users)) {
+        echo '<table class="wp-list-table widefat fixed striped users" style="width:100%;display:'.(!empty($list_users)?'block':'none').';">
             <thead>
                 <tr>
                     <th scope="col" id="title" class="manage-column column-author">Name</th>
@@ -164,20 +163,17 @@ class FGCQuizClass { /**
                     <th scope="col" id="action" class="manage-column column-author">Action</th>
                 </tr>
             </thead>
-            <tbody id="the-list">
-                <?php
-                foreach ( $list_users as $user ) {
-                    echo '<tr>';
+            <tbody id="the-list">';
+                if(!empty($list_users)) foreach ( $list_users as $user ) {
+                    echo '<tr id="'.$user->ID.'">';
                     echo '<td>' . esc_html( $user->user_nicename ) . '</td>';
                     echo '<td>' . esc_html( $user->user_email ) . '</td>';
                     echo '<td> <a href="#" onclick="removeMember('.$class_id.','.$user->ID.');return false;">Remove</a> </td>';
                     echo '</tr>';
                 }
-                ?>
-            </tbody>
-        </table>
-        <?php
-        }
+            echo '</tbody>
+        </table>';
+        //}
     }
     function ajax_action_member() {
         $type = isset($_REQUEST['type']) ? $_REQUEST['type'] : null;
@@ -194,6 +190,7 @@ class FGCQuizClass { /**
     }
     function add_member() {
         global $wpdb; // this is how you get access to the database
+        $response = ['status'=>0,'message'=>'No input'];
         $class_id = intval($_POST['class_id']);
         $query = !empty($_POST['member']) ? $_POST['member'] : null;
         $class = get_post($class_id);//, ARRAY_A 
@@ -202,14 +199,17 @@ class FGCQuizClass { /**
                 $user = get_user_by( 'email', $query );
             else
                 $user = get_user_by( 'login', $query );
-            if(!$user) echo 'User not found!';
+            if(!$user) $response['message'] = 'User not found!';
             else {
                 $user_name = $user->user_nicename or $user->user_email;
                 update_user_meta( $user->ID, '_class_id', $class->ID );
-                echo 'Add member '.$user_name.'('.$user->ID.') to class '.$class->post_title;
+                $response['status'] = 1;
+                //$response['message'] = 'Add member '.$user_name.'('.$user->ID.') to class '.$class->post_title;
+                $response['message'] = '<tr id="'.$user->ID.'"><td>'.$user->user_nicename.'</td><td>'.$user->user_email.'</td><td> <a href="#" onclick="removeMember('.$class->ID.','.$user->ID.');return false;">Remove</a> </td></tr>';
             }
             
-        } else echo 'Please enter username or email address!';
+        } else $response['message'] = 'Please enter username or email address!';
+        wp_send_json($response);
         wp_die(); // this is required to terminate immediately and return a proper response
     }
     function remove_member() {
@@ -218,11 +218,14 @@ class FGCQuizClass { /**
 
         $user_id = intval($_POST['user_id']);
         $user = get_user_by( 'ID', $user_id);
+        $response = ['status'=>0,'message'=>'No input'];
         if($user && $class) {
             $user_name = $user->user_nicename or $user->user_email;
             delete_user_meta( $user->ID, '_class_id');
-            echo 'Remove user '.$user_name.' form class '.$class->post_title.' success';
-        } else echo 'User or Class not found!';
+            $response['status'] = 1;
+            $response['message'] = 'Remove user '.$user_name.' form class '.$class->post_title.' success';
+        } else $response['message'] = 'User or Class not found!';
+        wp_send_json($response);
         wp_die();
     }
     function action_member_javascript() { ?>
@@ -237,7 +240,14 @@ class FGCQuizClass { /**
                     'member': member,
                 };
                 jQuery.post(ajaxurl, data, function(response) {
-                    alert('Got this from the server: ' + response);
+                    if(response.status===1) {
+                        jQuery('table.wp-list-table.users').show();
+                        jQuery('table.users tbody').append(response.message);
+                    } else if(response.status===0) {
+                        alert(response.message);
+                    } else {
+                        alert('Got this from the server: ' + response);
+                    }
                 });
             }
             function removeMember(class_id,user_id) {
@@ -248,12 +258,17 @@ class FGCQuizClass { /**
                     'user_id': user_id,
                 };
                 jQuery.post(ajaxurl, data, function(response) {
-                    alert('Got this from the server: ' + response);
+                    if(response.status===1) {
+                        jQuery('table.users tbody tr#'+user_id).remove();
+                    } else if(response.status===0) {
+                        alert(response.message);
+                    } else {
+                        alert('Got this from the server: ' + response);
+                    }
                 });
             }
         </script><?php
     }
-
 }
 
 new FGCQuizClass;
